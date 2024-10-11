c4f780991d03e4a7d42fcd9944d4668f038fd6ae
built-by-dhruv <dhruvvaishnav2125@gmail.com>
2024-10-12 05:27:34 +0530

    api file

--- service.py
+++ service.py
@@ -7,6 +7,6 @@ iris_clf_runner = bentoml.sklearn.get("iris_svm_model:latest").to_runner()
 svc = bentoml.Service("iris_classifier",runners=[iris_clf_runner])
 
 @svc.api(input = NumpyNdarray(),output =  NumpyNdarray())
-def classify(data: np.ndarray):
+def classify(data: np.ndarray) -> np.ndarray:
     # return prediction
-    return svc.predict.run(data)
+    return iris_clf_runner.predict.run(data) 
